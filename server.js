b8c17c63819a13357e90d649062de8c120ff8d13
Erez Horev <erez.horev@elastifile.com>
2017-10-07 00:30:49 +0300

    server.js: remove comments

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var express = require("express");
 var app     = express();
-// var fs = require('fs');
 
 app.use(express.static(__dirname + '/client'));
 
@@ -10,16 +9,3 @@ app.get('/',function(req,res){
   });
 
 app.listen(8080);
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, {"Content-Type": "text/html"});
-//     var data = fs.readFileSync('./client/index.html')
-//     res.write(data);
-//     var data = fs.readFileSync('./client/app.js')
-//     res.write(data);
-//     res.end();
-
-//     console.log("intercepted a request", req.url);
-//     console.log("");
-//     });
-
-// server.listen(8080);
