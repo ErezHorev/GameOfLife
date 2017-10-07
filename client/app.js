bb562b42b8a8a2f67151826e70650ac128b1d447
Erez Horev <erez.horev@elastifile.com>
2017-10-08 02:15:43 +0300

    client/app.js: namings and setting initial state of the grid

--- client/app.js
+++ client/app.js
@@ -1,15 +1,16 @@
 
 function game(canvas) {
     console.log("creating new game");
-    this.canvas = setCanvas(canvas);
-
-    this.cells = [];
+    this.canvas = setupCanvas(canvas);
+    this.grid = [];
     // methods
-    this.draw = draw;
     this.start = start;
+    this.tick = tick;
+    this.draw = draw;
+    this.update = update;
 }
 
-function setCanvas(canvas){
+function setupCanvas(canvas) {
     canvas.ctx = canvas.getContext('2d')
     canvas.ctx.strokeStyle = 'lightgray';
     canvas.ctx.fillStyle = 'black';
@@ -17,41 +18,55 @@ function setCanvas(canvas){
 }
 
 function start() {
-    rows = this.canvas.attributes.height.value
-    lines = this.canvas.attributes.width.value
+    // rows = this.canvas.attributes.height.value
+    // lines = this.canvas.attributes.width.value
+    rows = 100
+    lines = 100
+    this.grid = initCells(rows, lines);
+    this.tick()
+}
 
-    this.cells = initCells(rows, lines);
-    this.draw();
+function tick() {
+    var interval =  1000; //TODO: parameterize for user control
+    var canvas = this.canvas
+    var grid = this.grid
+    var drawAndUpdate = function(){
+        draw(canvas,grid)
+        update(grid)
+    }
+    setInterval(drawAndUpdate, interval);
 }
 
 function initCells(rows, lines) {
     // TODO: support common patterns like glider gun...etc
     console.log("Initializing cells on:", rows, "rows", "and", lines, "lines")
 
-    var cells = [[]];
+    var grid = [[]];
     for (row = 0; row < rows; row++) {
-        cells[row] = [];
+        grid[row] = [];
         for (line = 0; line < lines; line++) {
-            cells[row][line] = 0;
+            grid[row][line] = 0;
         }
     }
-    console.log(cells)
 
-    // cells.forEach(function (x, y) {
-    //     console.log(x, y)
-    // });
-    return cells
+    // set initial state
+    // TODO: parameterize initial pattern 
+    getGliderGun().forEach(function (coordinates) {
+        var x = coordinates[0], y = coordinates[1];
+        grid[x][y] = 1;
+    });
+
+    return grid
 }
 
-function draw() {
-    console.log("starting to draw on canvas...");
-    var canvas = this.canvas;
+function draw(canvas, grid) {
+    console.log("Drawing on canvas...");
+    // var canvas = this.canvas
+    // var grid = this.grid
     var cellSize = 10;
-
-    this.cells.forEach(function (row, x) {
-        row[x] = 1;
+    // TODO: transform this to some 'walk' method on cells (maybe rename 'cells' to 'grid'/'world'/'board'?)
+    grid.forEach(function (row, x) {
         row.forEach(function (cell, y) {
-            line[y] = 1;
             canvas.ctx.beginPath();
             canvas.ctx.rect(x * cellSize, y * cellSize, cellSize, cellSize);
             if (cell) {
@@ -61,4 +76,20 @@ function draw() {
             }
         });
     });
+    console.log("Done drawing on canvas...");
+}
+
+function update(grid) {
+    console.log("Updating cells...");
+}
+
+function getGliderGun() {
+    return [
+        // Gosper glider gun
+        [1, 5], [1, 6], [2, 5], [2, 6], [11, 5], [11, 6], [11, 7], [12, 4],
+        [12, 8], [13, 3], [13, 9], [14, 3], [14, 9], [15, 6], [16, 4],
+        [16, 8], [17, 5], [17, 6], [17, 7], [18, 6], [21, 3], [21, 4],
+        [21, 5], [22, 3], [22, 4], [22, 5], [23, 2], [23, 6], [25, 1],
+        [25, 2], [25, 6], [25, 7], [35, 3], [35, 4], [36, 3], [36, 4],
+    ]
 }
