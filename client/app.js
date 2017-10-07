55d9393011fd8fca58de2329c44caf82693e655f
Erez Horev <erez.horev@elastifile.com>
2017-10-08 00:06:23 +0300

    client: move logic from html to app.js + minor refactors

--- client/app.js
+++ client/app.js
@@ -1,32 +1,64 @@
 
 function game(canvas) {
     console.log("creating new game");
-    this.canvas = canvas;
+    this.canvas = setCanvas(canvas);
+
+    this.cells = [];
+    // methods
     this.draw = draw;
     this.start = start;
 }
 
+function setCanvas(canvas){
+    canvas.ctx = canvas.getContext('2d')
+    canvas.ctx.strokeStyle = 'lightgray';
+    canvas.ctx.fillStyle = 'black';
+    return canvas
+}
+
 function start() {
-    initCells();
+    rows = this.canvas.attributes.height.value
+    lines = this.canvas.attributes.width.value
+
+    this.cells = initCells(rows, lines);
     this.draw();
 }
 
-function initCells() {
-    // TODO: support special patterns like glider gun...etc
+function initCells(rows, lines) {
+    // TODO: support common patterns like glider gun...etc
+    console.log("Initializing cells on:", rows, "rows", "and", lines, "lines")
+
     var cells = [[]];
-    for (row = 0; row < 10; row++) {
+    for (row = 0; row < rows; row++) {
         cells[row] = [];
-        for (line = 0; line < 10; line++) {
-            cells[row][line] = 1;
+        for (line = 0; line < lines; line++) {
+            cells[row][line] = 0;
         }
     }
     console.log(cells)
 
-    cells.forEach(function (point) {
-        console.log(point[0], point[1])
-    });
+    // cells.forEach(function (x, y) {
+    //     console.log(x, y)
+    // });
+    return cells
 }
 
 function draw() {
-    console.log("starting to draw on canvas...")
+    console.log("starting to draw on canvas...");
+    var canvas = this.canvas;
+    var cellSize = 10;
+
+    this.cells.forEach(function (row, x) {
+        row[x] = 1;
+        row.forEach(function (cell, y) {
+            line[y] = 1;
+            canvas.ctx.beginPath();
+            canvas.ctx.rect(x * cellSize, y * cellSize, cellSize, cellSize);
+            if (cell) {
+                canvas.ctx.fill();
+            } else {
+                canvas.ctx.stroke();
+            }
+        });
+    });
 }
