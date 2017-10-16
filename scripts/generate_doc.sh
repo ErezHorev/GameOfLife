0840904201a9c6fb856180faca80bbbe42cc06e8
Erez Horev <erez.horev@elastifile.com>
2017-10-16 04:00:48 +0300

    move docs under assets

--- scripts/generate_doc.sh
+++ scripts/generate_doc.sh
@@ -6,4 +6,4 @@ if ! [ -x "$(command -v jsdoc)" ]; then
   echo -e 'Error: jsdoc is required and not installed !\n\nTo install run:   "npm install -g jsdoc"' >&2
   exit 1
 fi
-jsdoc ../assets -d ../doc
+jsdoc ../assets -d ../assets/docs
